a6f47e481febf1342bb464b94aa36e7872e3db71
Carolin Horn <51193493+lionarc@users.noreply.github.com>
2026-02-04 10:46:36 +0100

    feat: add javascript logic

--- grid.js
+++ grid.js
@@ -3,13 +3,30 @@ let y = 16;
 
 const rowContainer = document.querySelector(".row-container");
 
+function colorOn(event) {
+  const target = event.target;
+    if (target.classList.contains("col")) { 
+        target.style.backgroundColor = "black";
+    }
+}
+
+function colorOff(event) {
+  const target = event.target;
+    if (target.classList.contains("col")) { 
+        target.style.backgroundColor = "white";
+    }
+}
+
 for (let i = 0; i < x; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
+    row.addEventListener("mouseover", colorOn)
+    row.addEventListener("mouseout", colorOff)
     rowContainer.appendChild(row);
+    for (let j = 0; j < y; j++) {
+        const col = document.createElement("div");
+        col.classList.add("col");
+        row.appendChild(col);
+    }
     
 }
-
-
-
-
