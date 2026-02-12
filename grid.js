bac473fdca3321905f32bde9ee9d4e0232e54e20
Carolin Horn <51193493+lionarc@users.noreply.github.com>
2026-02-12 14:58:49 +0100

    feat: implement dynamic grid size adjustment and add button for user input

--- grid.js
+++ grid.js
@@ -2,6 +2,32 @@ let x = 16;
 let y = 16;
 
 const rowContainer = document.querySelector(".row-container");
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+    chosenSize = prompt("Enter the number of rows and columns (max 100):");
+    if (chosenSize > 100) {
+        alert("Please enter a number less than or equal to 100.");
+        return;
+    }
+    x = chosenSize;
+    y = chosenSize;
+    rowContainer.innerHTML = "";        
+    for (let i = 0; i < x; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.addEventListener("mouseover", colorOn)
+        row.addEventListener("mouseout", colorOff)
+        rowContainer.appendChild(row);
+        for (let j = 0; j < y; j++) {
+            const col = document.createElement("div");
+            col.classList.add("col");
+            row.appendChild(col);
+        }
+        
+    }
+});
+
 
 function colorOn(event) {
   const target = event.target;
